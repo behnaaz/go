15883128cf0527cd8df758daa1d87d4fe0aa63f5
Behnaz <behnaz.changizi@gmail.com>
2020-09-19 19:32:06 +0100

    Add get, post, and put methods

--- main.go
+++ main.go
@@ -7,6 +7,36 @@ import (
 	"net/http"
 )
 
+func get(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message" : "get called"}`))
+}
+
+func put(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message" : "put called"}`))
+}
+
+func post(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message" : "post called"}`))
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(`{"message" : "not found"}`))
+}
+
 func main() {
 	fmt.Println("vim-go")
+	r := mux.NewRouter()
+	r.HandleFunc("/", get).Methods(http.MethodGet)
+	r.HandleFunc("/", put).Methods(http.MethodPut)
+	r.HandleFunc("/", post).Methods(http.MethodPost)
+	r.HandleFunc("/", notFound)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
